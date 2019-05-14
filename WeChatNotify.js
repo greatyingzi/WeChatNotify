8b2b7a6bf86fa5dac1bfa82450f18eef12b6d93f
greatyingzi <924068818@qq.com>
2019-05-14 17:13:50 +0800

    Update WeChatNotify.js
    修改版本号为0.11

--- WeChatNotify.js
+++ WeChatNotify.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         WeChat Notify
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.11
 // @license      MIT
 // @description  try to take over the world!
 // @author       影子
